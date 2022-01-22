30b787cd352174893f1e79ed809a197146cb48e8
Victor Rodrigues <aniebson@gmail.com>
2022-01-22 16:32:49 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/victorrodrigues/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,43 @@
+package com.victorrodrigues.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.victorrodrigues.cursomc.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto extends Pagamento {
+	private static final long serialVersionUID = 1L;
+
+	private Date dataVencimento;
+	private Date dataPagameto;
+
+	public PagamentoComBoleto() {
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagameto) {
+		super(id, estado, pedido);
+		this.dataVencimento = dataVencimento;
+		this.dataPagameto = dataPagameto;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagameto() {
+		return dataPagameto;
+	}
+
+	public void setDataPagameto(Date dataPagameto) {
+		this.dataPagameto = dataPagameto;
+	}
+	
+	
+
+}
