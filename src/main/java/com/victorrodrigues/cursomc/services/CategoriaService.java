745f37910cd7228ebe0a2e5c7be22e291d8952cb
Victor Rodrigues <aniebson@gmail.com>
2022-01-25 22:08:35 -0300

    Listando todas categorias com DTO

--- src/main/java/com/victorrodrigues/cursomc/services/CategoriaService.java
+++ src/main/java/com/victorrodrigues/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.victorrodrigues.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,12 +36,15 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		find(id);
-
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
 		}
-
 	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
+
 }
