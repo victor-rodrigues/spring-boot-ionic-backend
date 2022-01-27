431583e16887bf0301d41245fd96be1df2e38179
Victor Rodrigues <aniebson@gmail.com>
2022-01-26 22:54:01 -0300

    Personalizado o retorno da validacao

--- src/main/java/com/victorrodrigues/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/victorrodrigues/cursomc/dto/CategoriaDTO.java
@@ -11,9 +11,8 @@ import com.victorrodrigues.cursomc.domain.Categoria;
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-
 	private Integer id;
-	
+
 	@NotEmpty(message = "Preenchimento obrigatório")
 	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
