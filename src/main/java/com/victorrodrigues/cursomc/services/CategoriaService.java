95d78be400c1e1a545104935647dc55989851536
Victor Rodrigues <aniebson@gmail.com>
2022-01-26 22:26:01 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/victorrodrigues/cursomc/services/CategoriaService.java
+++ src/main/java/com/victorrodrigues/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.victorrodrigues.cursomc.domain.Categoria;
+import com.victorrodrigues.cursomc.dto.CategoriaDTO;
 import com.victorrodrigues.cursomc.repositories.CategoriaRepository;
 import com.victorrodrigues.cursomc.services.exceptions.DataIntegrityException;
 import com.victorrodrigues.cursomc.services.exceptions.ObjectNotFoundException;
@@ -49,10 +50,14 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
